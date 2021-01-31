3e4f1b8ce731631f5b8a0d626d20ba940015e44b
Darshil Vaishnav <darshilvaishnav69@gmail.com>
2021-01-31 20:54:04 +0530

    added port,start script,connection string

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
 
-mongoose.connect("mongodb://localhost:27017/test",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mongoose.connect("mongodb+srv://duser:1234@cluster0.cxupx.mongodb.net/college?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app = express();
         app.use("/api",route);
-        app.listen(3000,()=>{
+        app.listen(process.env.PORT || 3000,()=>{
             console.log("server has started on 3000");
         });
     }
